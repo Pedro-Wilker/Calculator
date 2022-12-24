1e93a1140c0c722f808e383cceabdc8641d25304
unknown <81252209+Pedrowww170@users.noreply.github.com>
2022-12-24 20:43:25 -0300

    Adding numbers, buttons and functionality in pages JS

---
+++ js/buttons/sumButtons.js
@@ -0,0 +1,10 @@
+var buttonSum = document.getElementById("buttonSum");
+
+function buttonNewSum(acao, tipo ){
+
+    var tipo = "+";
+    console.log("teste");
+}
+
+
+buttonSum.addEventListener("click", buttonNewSum);
---
+++ js/numbers/numbers.js
@@ -0,0 +1,80 @@
+var numberNine = document.getElementById("numberNine");
+var numberEight = document.getElementById("numberEight");
+var numberSeven = document.getElementById("numberSeven");
+var numberSix = document.getElementById("numberSix");
+var numberFive = document.getElementById("numberFive");
+var numberFour = document.getElementById("numberFour");
+var numberThree = document.getElementById("numberThree");
+var numberTwo = document.getElementById("numberTwo");
+var numberOne = document.getElementById("numberOne");
+var numberZero = document.getElementById("numberZero");
+
+function numberNewNine(valor, number9 ){
+
+    var number9 = 9;
+    console.log("teste 9");
+}
+
+function numberNewEigth(valor, number8 ){
+
+    var number8 = 8;
+    console.log("teste 8");
+}
+
+function numberNewSeven(valor, number7 ){
+
+    var number7 = 7;
+    console.log("teste 7");
+}
+
+function numberNewSix(valor, number6 ){
+
+    var number6 = 6;
+    console.log("teste 6");
+}
+
+function numberNewFive(valor, number5 ){
+
+    var number5 = 5;
+    console.log("teste 5");
+}
+
+function numberNewFour(valor, number4 ){
+
+    var number4 = 4;
+    console.log("teste 4");
+}
+
+function numberNewThree(valor, number3 ){
+    var number3 = 3;
+    console.log("teste 3");
+}
+
+function numberNewTwo(valor, number2 ){
+
+    var number2 = 2;
+    console.log("teste 2");
+}
+
+function numberNewOne(valor, number1 ){
+
+    var number1 = 1;
+    console.log("teste 1");
+}
+
+function numberNewZero(valor, number0 ){
+
+    var number0  = 0;
+    console.log("teste 0");
+}
+
+numberNine.addEventListener("click", numberNewNine);
+numberEight.addEventListener("click", numberNewEigth);
+numberSeven.addEventListener("click", numberNewSeven);
+numberSix.addEventListener("click", numberNewSix);
+numberFive.addEventListener("click", numberNewFive);
+numberFour.addEventListener("click", numberNewFour);
+numberThree.addEventListener("click", numberNewThree);
+numberTwo.addEventListener("click", numberNewTwo);
+numberOne.addEventListener("click", numberNewOne);
+numberZero.addEventListener("click", numberNewZero);
