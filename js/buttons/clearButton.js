c795b7524680b17ffefbcda4b9b4d68fdbf933db
unknown <81252209+Pedrowww170@users.noreply.github.com>
2022-12-24 17:47:52 -0300

    Add numbers and buttons in pages JS

---
+++ js/buttons/clearButton.js
@@ -0,0 +1,10 @@
+var buttonClear = document.getElementById("buttonClear");
+
+function buttonNewClear(acao, tipo ){
+
+    var tipo = "c";
+    console.log("teste");
+}
+
+
+buttonClear.addEventListener("click", buttonNewClear);
---
+++ js/buttons/mutiplicButton.js
@@ -0,0 +1,9 @@
+var buttonMutiplic = document.getElementById("buttonMutiplic");
+
+function buttonNewMutiplic(acao, tipo ){
+
+    var tipo = "x";
+    console.log("teste");
+}
+
+buttonMutiplic.addEventListener("click", buttonNewMutiplic);
